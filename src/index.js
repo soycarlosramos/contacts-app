0ee56725269b8f40c95411cb27ddb207e8a85771
Carlos Mario Ramos Pérez <carlosmarioramos34@gmail.com>
2023-05-28 18:12:51 -0500

    Init server

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import { dirname, join, resolve } from 'path'
+import { fileURLToPath } from 'url'
+import { config } from 'dotenv'
+import morgan from 'morgan'
+
+config({ path: join(process.cwd(), `.env.${process.env.NODE_ENV}`) })
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+const app = express()
+
+app.set('port', process.env.PORT || 3000)
+app.set('views', resolve(__dirname, 'views'))
+app.set('tamplate engine', 'pug')
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(morgan('dev'))
+
+app.listen(app.get('port'), () => {
+  if (process.env.NODE_ENV === 'production' ) {
+    console.log('Node server running...')
+    return
+  }
+  
+  console.log(`Node server running on http://localhost:${app.get('port')}`)
+  console.log(`Try http://localhost:${app.get('port')}`)
+})
