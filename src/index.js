6b4664f5eba1813ba4b6bbaea4751fc88ec2eea6
Carlos Mario Ramos Pérez <carlosmarioramos34@gmail.com>
2023-05-28 19:16:48 -0500

    Fix views and public folders and paths and set static content

--- src/index.js
+++ src/index.js
@@ -12,8 +12,9 @@ const app = express()
 
 app.set('port', process.env.PORT || 3000)
 app.set('view engine', 'pug')
-app.set('views', join(__dirname, './public/views'))
+app.set('views', join(__dirname, 'views'))
 
+app.use(express.static(join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(morgan('dev'))
